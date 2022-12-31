0f8c80c2a9927a3e5f1c3e8d8b591711d558c1a8
jonathan <bayart.jonathan@laposte.net>
2022-12-31 09:57:38 +0100

    introduction api

---
+++ app.js
@@ -0,0 +1,25 @@
+//  https://api.blablagues.net/?rub=blagues
+
+const header = document.getElementById("header");
+const content = document.getElementById("content");
+
+// console.log(header, content);
+
+function getJoke() {
+
+    fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data.data.content);
+
+        header.textContent = data.data.content.text_head;
+        content.textContent = data.data.content.text !== ""
+        ? data.data.content.text
+        : data.data.content.text_hidden;
+    });
+}
+getJoke();
+
+document.body.addEventListener("click", () => {
+    getJoke();
+})
